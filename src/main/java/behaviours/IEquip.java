e66279450535bc994cfec468b4e923b2ae7a9c30
alexandergm1 <alexander.1gmac@gmail.com>
2022-02-16 13:32:27 +0000

    behaviours package and IEquip interface setup completed

---
+++ src/main/java/behaviours/IEquip.java
@@ -0,0 +1,5 @@
+package behaviours;
+
+public interface IEquip {
+    void equip(IEquip item);
+}
--- src/main/java/quest/Quest.java
+++ src/main/java/quest/Quest.java
@@ -1,14 +1,41 @@
 package quest;
+import enemies.Ringwraith;
 import players.Player;
 import rooms.Room;
+import rooms.Weathertop;
+
 import java.util.ArrayList;
 
 public class Quest {
     private ArrayList<Room> levels;
     private ArrayList<Player> players;
+    private Room currentRoom;
 
-    public Quest(ArrayList<Room> levels, ArrayList<Player> players) {
+
+//    Ringwraith ringwraith = new Ringwraith(5, "Khamûl")
+
+
+//    private Weathertop weathertop = new Weathertop(ArrayList<Ringwraith>, );
+
+    public Quest(ArrayList<Room> levels, ArrayList<Player> players, Room currentRoom) {
         this.levels = levels;
         this.players = new ArrayList<>();
+        this.currentRoom = levels.get(0);
+    }
+
+    public ArrayList<Room> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(ArrayList<Room> levels) {
+        this.levels = levels;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
     }
 }
