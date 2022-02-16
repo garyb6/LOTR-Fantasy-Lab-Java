dc05969d9c6ad79c6661e4278359d0b65a31e541
alexandergm1 <alexander.1gmac@gmail.com>
2022-02-16 11:12:03 +0000

    Player abstract class setup

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -1,4 +1,34 @@
 package players;
 
 public abstract class Player {
+
+    private double hp;
+    private String name;
+    private int power;
+
+    public Player(double hp, String name, int power) {
+        this.hp = hp;
+        this.name = name;
+        this.power = power;
+    }
+
+    public double getHp() {
+        return hp;
+    }
+
+    public void setHp(double hp) {
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeDamage(int damage){
+        setHp(this.hp -= damage);
+    }
 }
